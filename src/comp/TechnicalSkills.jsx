dc8d83d3f4985ffd8b0fd1fcef6702a5b97c95c8
suryadevarapavan <suryadevarapavan770@gmail.com>
2025-10-20 08:56:41 +0530

    refactoring-2

--- src/comp/TechnicalSkills.jsx
+++ src/comp/TechnicalSkills.jsx
@@ -1,5 +1,5 @@
 
-// TechnicalSkills.jsx
+//TechnicalSkills.jsx
 import React from "react";
 import "./TechnicalSkills.css";
 
@@ -9,60 +9,82 @@ function TechnicalSkills() {
       <div className="skills-card">
         <h2 className="skills-title">Technical Skills</h2>
 
-        {/* Languages */}
         <div className="skills-section">
           <h3 className="skills-subtitle">Languages</h3>
           <div className="icon-grid">
             <div className="icon-link">
               <img src="python.svg" alt="Python" />
+              <span>Python</span>
             </div>
             <div className="icon-link">
               <img src="go.svg" alt="Go" />
+              <span>Go</span>
             </div>
             <div className="icon-link">
               <img src="javascript.svg" alt="JavaScript" />
+              <span>JavaScript</span>
+            </div>
+            <div className="icon-link">
+              <img src="html.svg" alt="HTML" />
+              <span>HTML</span>
+            </div>
+            <div className="icon-link">
+              <img src="css.svg" alt="CSS" />
+              <span>CSS</span>
             </div>
           </div>
         </div>
 
-        {/* Databases */}
         <div className="skills-section">
           <h3 className="skills-subtitle">Databases</h3>
           <div className="icon-grid">
             <div className="icon-link">
               <img src="mysql.svg" alt="MySQL" />
+              <span>MySQL</span>
             </div>
             <div className="icon-link">
               <img src="mariadb.svg" alt="MariaDB" />
+              <span>MariaDB</span>
             </div>
             <div className="icon-link">
               <img src="mongodb.svg" alt="MongoDB" />
+              <span>MongoDB</span>
+            </div>
+            <div className="icon-link">
+              <img src="postgresql.svg" alt="PostgreSQL" />
+              <span>PostgreSQL</span>
             </div>
           </div>
         </div>
 
-        {/* Tools & Frameworks */}
         <div className="skills-section">
-          <h3 className="skills-subtitle">Tools & Frameworks</h3>
+          <h3 className="skills-subtitle">Frameworks</h3>
           <div className="icon-grid">
             <div className="icon-link">
               <img src="react.svg" alt="React.js" />
+              <span>React.js</span>
             </div>
             <div className="icon-link">
               <img src="git.svg" alt="Git" />
+              <span>Git</span>
             </div>
           </div>
         </div>
 
-        {/* Platforms */}
         <div className="skills-section">
           <h3 className="skills-subtitle">Platforms</h3>
           <div className="icon-grid">
             <div className="icon-link">
               <img src="linux.svg" alt="Linux" />
+              <span>Linux</span>
             </div>
             <div className="icon-link">
-              <img src="gitforwindows.svg" alt="Windows" />
+              <img src="windows.svg" alt="Windows" />
+              <span>Windows</span>
+            </div>
+            <div className="icon-link">
+              <img src="gcloud.svg" alt="Google Cloud" />
+              <span>Google Cloud</span>
             </div>
           </div>
         </div>
